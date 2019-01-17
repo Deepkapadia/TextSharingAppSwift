207de818dd3deaed10484af04c751f682040416c
Deep_Kapadia <kapadia.deep12@gmail.com>
2019-01-17 18:35:36 +0530

    Text_Sharing_App_Swift
    Text Sharing App Swift Sample

--- TextSharingAppSwift/ViewController.swift
+++ TextSharingAppSwift/ViewController.swift
@@ -20,6 +20,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func shareclick(_ sender: Any) {
+        ShareApp();
+    }
+    
+    func ShareApp(text:String? = nil,img:UIImage? = nil,url:URL? = nil){
+        
+        var arr=Array<Any>();
+        
+        if text != "" {
+            
+            arr.append(text);
+        }
+        if img != nil {
+            
+            arr.append(img);
+        }
+        if url != nil {
+            
+            arr.append(url);
+        }
+        
+        var activity = UIActivityViewController(activityItems: arr, applicationActivities: nil);
+        
+        self.present(activity, animated: true, completion: nil);
+    }
 
 }
 
